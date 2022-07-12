9a3315f1b36c3d7c4e8e4a3e1f307df8ecb871d7
pilarleminski <pilar.leminski@gmail.com>
2022-07-12 13:21:52 -0300

    Primeiro commit

---
+++ src/loops/nomeIdade.java
@@ -0,0 +1,27 @@
+package loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que leia um conjunto de dois valores, o primeiro representando o nome do aluno
+e o segundo representando sua idade.
+(Pare inserindo o valor 0 no campo nome)
+ */
+public class nomeIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true){
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if(nome.equals("0")) break;
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+
+        }
+        System.out.println("FIM DO PROGRAMA");
+    }
+}
