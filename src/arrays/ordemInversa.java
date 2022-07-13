82010e5de538f887e5dcc9ee412ff0508659c248
pilarleminski <pilar.leminski@gmail.com>
2022-07-13 16:46:34 -0300

    Completo

--- src/arrays/ordemInversa.java
+++ src/arrays/ordemInversa.java
@@ -1,4 +1,19 @@
 package arrays;
-
+ /*
+ Crie um vetor de 6 números e mostre-os na ordem inversa
+  */
 public class ordemInversa {
+    public static void main(String[] args) {
+        int[] vetor = {1, 2, 3, 4, 5, 6};
+        int count = 0;
+        while(count < vetor.length){
+            System.out.println(vetor[count]);
+            count++;
+        }
+
+        System.out.println("Vetor inverso: ");
+        for(int i = vetor.length - 1; i >= 0; i--){
+            System.out.println(vetor[i]);
+        }
+    }
 }
